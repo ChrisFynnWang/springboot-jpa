cb2d3b66770a93418865e2535b4f6843722c4ea7
FynnWang <wftop1@126.com>
2019-04-16 23:55:45 +0800

    jpa項目初始化

--- src/main/java/com/wangfan/springboot/jpa/service/impl/UserServiceImpl.java
+++ src/main/java/com/wangfan/springboot/jpa/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.wangfan.springboot.jpa.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.wangfan.springboot.jpa.entity.User;
 import com.wangfan.springboot.jpa.repository.UserRepository;
 import com.wangfan.springboot.jpa.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 
 /**
@@ -19,11 +24,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService
 {
+    private static final String USER_KEY = "USER_";
+
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @Transactional(rollbackFor = Exception.class,propagation = Propagation.REQUIRED)
     public User getUser(int userId)
     {
-        return userRepository.findOne(userId);
+        String redisStr = redisTemplate.opsForValue().get(USER_KEY + userId);
+        if (!StringUtils.isEmpty(redisStr)) {
+
+            return JSON.parseObject(redisStr, User.class);
+        }
+        User one = userRepository.findById(userId).get();
+        if (null != one) {
+            redisTemplate.opsForValue().set(USER_KEY + one.getUserId(),JSON.toJSONString(one));
+        }
+        return one;
     }
 }
